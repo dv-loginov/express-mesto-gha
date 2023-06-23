43eb68649b68178dc6edb7922fd2510eb04fb63f
dv-loginov <dilog@rambler.ru>
2023-06-23 09:33:59 +0300

    feat: Make the name, about, and avatar fields optional. Give them default values.

--- models/user.js
+++ models/user.js
@@ -6,23 +6,23 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   name: {
     type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
+    default: 'Жак-Ив Кусто',
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
   },
   about: {
     type: String,
-    required: [true, 'Поле "about" должно быть заполнено'],
+    default: 'Исследователь',
     minlength: [2, 'Минимальная длина поля "about" - 2'],
     maxlength: [30, 'Максимальная длина поля "about" - 30'],
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (v) => validator.isURL(v),
       message: 'Некорректный URL',
     },
-    required: [true, 'Поле "avatar" должно быть заполнено'],
   },
   email: {
     type: String,
